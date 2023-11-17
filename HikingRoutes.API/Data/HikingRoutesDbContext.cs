c89ef6a6d402d92490ae83ae32fe7751ddcd72e2
vikto <vikto@DESKTOP-VSMI7MP>
2023-11-17 17:05:44 +0200

    Added dbContext and entity tables. Added conncection string. Added initial migration.

---
+++ HikingRoutes.API/Data/HikingRoutesDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using HikingRoutes.API.Models.Domain;
+
+
+namespace HikingRoutes.API.Data
+{
+    public class HikingRoutesDbContext : DbContext
+    {
+        public HikingRoutesDbContext(DbContextOptions<HikingRoutesDbContext> dbContextOptions) 
+                                    : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; } = null!;
+
+        public DbSet<Region> Regions { get; set; } = null!;
+
+        public DbSet<HikingRoutes.API.Models.Domain.Route> Routes { get; set; }
+
+
+
+
+
+    }
+}
