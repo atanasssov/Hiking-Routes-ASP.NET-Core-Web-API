47c22d6083127f7a1b332010fd9ee71339f07830
vikto <v.atanassovv@gmail.com>
2023-11-21 18:29:19 +0200

    Seed roles to DB

--- HikingRoutes.API/Data/HikingRoutesAuthDbContext.cs
+++ HikingRoutes.API/Data/HikingRoutesAuthDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace HikingRoutes.API.Data
 {
@@ -8,7 +9,37 @@ namespace HikingRoutes.API.Data
         public HikingRoutesAuthDbContext(DbContextOptions <HikingRoutesAuthDbContext> dbContextOptions) 
                                         : base(dbContextOptions)
         {
-
+            
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+
+            string? readerRoleId = "da9acf9e-4fc0-4df8-b9d1-ff7ace245c73";
+            string? writerRoleId = "db1280ed-0eaa-441d-af34-15f632800e75";
+
+            ICollection<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }   
+        
     }
 }
